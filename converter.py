2be08c78d788904e530f1f4d8d01a6e10acba59a
Luke Karter <luke.karter@jhgs.bucks.sch.uk>
2024-09-19 15:14:05 +0100

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,21 @@
+# converter made by luke karter
+# binary to denary
+def b2dConvert():
+  # placeholder
+# denary to binary
+def d2bConvert():
+  number=int(input("Enter the denary number you will convert to binary")
+  if number>255 or number<0:
+    print("This number is signed or cannot be recorded in 8 bits!")
+  else:
+    # placeholder
+# main
+inputType=2
+print("This is a 8 bit unsigned binary to denary converter.")
+while inputType != 0 or 1:
+  inputType=int(input("Enter 0 for binary to denary, or 1 for denary to binary: "))
+if inputType=0:
+  b2dConvert
+else:
+  d2bConvert
+print("Your result is "+result+"!")
